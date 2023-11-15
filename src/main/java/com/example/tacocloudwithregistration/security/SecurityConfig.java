594d6a56e2d8de1786539507a81d96caf6e0a606
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-11-15 12:49:56 +0300

    commit

---
+++ src/main/java/com/example/tacocloudwithregistration/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.tacocloudwithregistration.security;
+
+import com.example.tacocloudwithregistration.data.User;
+import com.example.tacocloudwithregistration.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null) {
+                return user;
+            } else {
+                throw new UsernameNotFoundException("User '" + username + "' not found");
+            }
+        };
+    }
+}
