762d4b45c1d75c6d892eb6bfacea7fa02b35d14c
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-11-15 12:11:24 +0300

    commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,37 @@
+drop sequence taco_sequence if exists;
+drop sequence taco_order_sequence if exists;
+create sequence taco_sequence start with 1 increment by 50;
+create sequence taco_order_sequence start with 1 increment by 50;
+drop table Taco_Order if exists cascade;
+drop table Taco if exists cascade;
+drop table Ingredient_Ref if exists cascade;
+drop table Ingredient if exists cascade;
+create table if not exists Taco_Order (
+    id identity(1, 1) primary key,
+    delivery_Name varchar(50) not null,
+    delivery_Street varchar(50) not null,
+    delivery_City varchar(50) not null,
+    delivery_State varchar(50) not null,
+    delivery_Zip varchar(20) not null,
+    cc_number varchar(16) not null,
+    cc_expiration varchar(5) not null,
+    cccvv varchar(3) not null,
+    placed_at timestamp not null
+);
+create table if not exists Taco (
+    id identity(1, 1) primary key,
+    name varchar(50) not null,
+    taco_order_id bigint,
+    created_at timestamp not null
+);
+create table if not exists Ingredient_Ref (
+    taco_id bigint not null,
+    ingredient_id varchar(4) not null
+);
+create table if not exists Ingredient (
+    id varchar(4) primary key,
+    name varchar(50) not null,
+    type varchar(10) not null
+);
+alter table Ingredient_Ref add foreign key (ingredient_id) references Ingredient(id);
+alter table Ingredient_Ref add foreign key (taco_id) references Taco(id);
