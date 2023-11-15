5419f487970fb585a94a6873c8cfe661f795d113
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-11-15 12:51:21 +0300

    commit

--- src/main/java/com/example/tacocloudwithregistration/repository/OrderRepository.java
+++ src/main/java/com/example/tacocloudwithregistration/repository/OrderRepository.java
@@ -1,6 +1,6 @@
 package com.example.tacocloudwithregistration.repository;
 
-import com.example.tacocloudwithjpa.data.TacoOrder;
+import com.example.tacocloudwithregistration.data.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
