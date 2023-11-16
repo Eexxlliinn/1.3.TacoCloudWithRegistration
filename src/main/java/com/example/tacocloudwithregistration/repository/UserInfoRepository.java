7caac1892811f25d6fbb82f37df45b4e017cb707
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-11-16 14:30:03 +0300

    commit

--- src/main/java/com/example/tacocloudwithregistration/repository/UserInfoRepository.java
+++ src/main/java/com/example/tacocloudwithregistration/repository/UserInfoRepository.java
@@ -1,10 +1,10 @@
 package com.example.tacocloudwithregistration.repository;
 
-import com.example.tacocloudwithregistration.data.User;
+import com.example.tacocloudwithregistration.data.UserInfo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
+public interface UserInfoRepository extends CrudRepository<UserInfo, Long> {
+    UserInfo findByUsername(String username);
 }
