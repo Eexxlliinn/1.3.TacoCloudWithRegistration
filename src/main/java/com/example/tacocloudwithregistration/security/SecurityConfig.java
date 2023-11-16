3101d571bf107bbb7f31db28c53386dacdf57c7b
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-11-16 14:28:39 +0300

    commit

--- src/main/java/com/example/tacocloudwithregistration/security/SecurityConfig.java
+++ src/main/java/com/example/tacocloudwithregistration/security/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.example.tacocloudwithregistration.security;
 
-import com.example.tacocloudwithregistration.data.User;
-import com.example.tacocloudwithregistration.repository.UserRepository;
+import com.example.tacocloudwithregistration.data.UserInfo;
+import com.example.tacocloudwithregistration.repository.UserInfoRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 public class SecurityConfig {
@@ -17,14 +21,31 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepo) {
+    public UserDetailsService userDetailsService(UserInfoRepository userInfoRepo) {
         return username -> {
-            User user = userRepo.findByUsername(username);
-            if (user != null) {
-                return user;
+            UserInfo userInfo = userInfoRepo.findByUsername(username);
+            if (userInfo != null) {
+                return userInfo;
             } else {
                 throw new UsernameNotFoundException("User '" + username + "' not found");
             }
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvc = new MvcRequestMatcher.Builder(introspector);
+        http
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers(mvc.pattern("/design"), mvc.pattern("/orders")).hasRole("USER")
+                .requestMatchers(mvc.pattern("/"), mvc.pattern("/**")).permitAll()
+                );
+        http
+                .formLogin(form -> form
+                    .loginPage("/login")
+                    .permitAll()
+                    .defaultSuccessUrl("/design")
+                );
+        return http.build();
     }
 }
