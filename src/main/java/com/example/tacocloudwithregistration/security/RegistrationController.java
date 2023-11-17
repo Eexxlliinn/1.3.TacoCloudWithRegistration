a54fe3968d354272a90b2154eacf5290d94f73b8
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-11-17 11:01:37 +0300

    commit

--- src/main/java/com/example/tacocloudwithregistration/security/RegistrationController.java
+++ src/main/java/com/example/tacocloudwithregistration/security/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.example.tacocloudwithregistration.security;
 
 import com.example.tacocloudwithregistration.repository.UserInfoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/register")
 public class RegistrationController {
@@ -27,7 +29,9 @@ public class RegistrationController {
 
     @PostMapping
     public String processRegistration(RegistrationForm form) {
+        log.info("Form info: {}", form);
         userInfoRepo.save(form.toUserInfo(passwordEncoder));
+        log.info("User info: {}", userInfoRepo.findByUsername("Eugene"));
         return "redirect:/login";
     }
 }
