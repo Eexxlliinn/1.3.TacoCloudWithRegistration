fa06e704e9a49f8e49cf600cb60d4b97eace1615
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-11-26 03:08:42 +0300

    commit

--- src/main/java/com/example/tacocloudwithregistration/security/SecurityConfig.java
+++ src/main/java/com/example/tacocloudwithregistration/security/SecurityConfig.java
@@ -6,21 +6,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.header.writers.frameoptions.WhiteListedAllowFromStrategy;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import java.util.Arrays;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
+
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -44,20 +51,36 @@ public class SecurityConfig {
         MvcRequestMatcher.Builder mvc = new MvcRequestMatcher.Builder(introspector);
         http
                 .authorizeHttpRequests(auth -> auth
-                .requestMatchers(mvc.pattern("/design"), mvc.pattern("/orders")).hasRole("USER")
                 .requestMatchers(mvc.pattern("/"), mvc.pattern("/**"), mvc.pattern("/h2-console/**")).permitAll()
                 .requestMatchers(toH2Console()).permitAll()
+                .requestMatchers(mvc.pattern("/design"), mvc.pattern("/orders")).hasRole("USER")
                 .anyRequest().authenticated()
                 )
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers(mvc.pattern("/h2-console/**"))  // disable CSRF for H2
+                        .csrfTokenRepository(csrfTokenRepository())
                         .ignoringRequestMatchers(toH2Console())  // disable CSRF for H2
+                        .ignoringRequestMatchers(mvc.pattern("/h2-console/**"))  // disable CSRF for H2
                 )
                 .formLogin(form -> form
                     .loginPage("/login")
                     .permitAll()
                     .defaultSuccessUrl("/design")
+                )
+                .headers(headers ->
+                        headers
+                                .frameOptions(frameOptions -> frameOptions.disable())
+                )
+                .logout(logout ->
+                        logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .logoutSuccessUrl("/")
+                                .permitAll()
                 );
         return http.build();
+    }
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setSessionAttributeName("_csrf");
+        return repository;
     }
 }
